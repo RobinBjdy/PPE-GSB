98605638608083d3cd14a23f03354b2d4e44f47a
Robin BIJAUDY <robin.bijaudy@gmail.com>
2020-11-03 17:20:56 +0100

    Validation fiche de frais (en cours)

--- vues/v_ValiderFicheDeFrais.php
+++ vues/v_ValiderFicheDeFrais.php
@@ -129,7 +129,7 @@
         <input id="annuler" type="reset" value="Réinitialiser" class="btn btn-danger" 
                    role="button"></br> </br>
         <div class="panel panel-info">
-        <div class="panel-heading">Eléments forfaistisés</div>
+        <div class="panel-heading">Eléments forfaitisés</div>
         <table class="table table-bordered table-responsive">
             <tr>
                 <th>Libelle</th>
