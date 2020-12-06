b3562754128360916461f087d481fa3f56f7c0d1
jean Eude <jean Eude@Robin>
2020-12-06 17:49:29 +0100

    Mise en place des case supprimer fraisHF, supprimer et reporter

--- controleurs/c_SuivrePaiement.php
+++ controleurs/c_SuivrePaiement.php
@@ -8,13 +8,19 @@
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 $idVisiteur = filter_input(INPUT_POST, 'lstVisiteur', FILTER_SANITIZE_STRING);
 $montants = 0;
+$date = date('Ym');
 switch ($action) {
     case 'selectionnerVisiteur' :
-        $date = date('Ym');
-        $_SESSION['date'] = $date;
-        $lesVisiteur = $pdo->getVisiteurFromMoisVA($date);
-        $selectedValue = $lesVisiteur[0];
-        include 'vues/v_SelectVisiteurSP.php';
+        if (empty($pdo->getVisiteurFromMoisVA($date))) {
+            ajouterErreur("Aucun visiteur n'a de fiche de frais ce mois ci");
+            include 'vues/v_erreurs.php';
+            include 'vues/v_SelectVisiteurSP.php';
+        } else {
+            $_SESSION['date'] = $date;
+            $lesVisiteur = $pdo->getVisiteurFromMoisVA($date);
+            $selectedValue = $lesVisiteur[0];
+            include 'vues/v_SelectVisiteurSP.php';
+        }
         break;
     case 'FicheFraisSP':
         $leVisiteur = filter_input(INPUT_POST, 'lstVisiteur', FILTER_SANITIZE_STRING);
@@ -28,7 +34,6 @@ switch ($action) {
         $infoFicheDeFrais = $pdo->getLesInfosFicheFrais($_SESSION['visiteur'], $_SESSION['date']);
         $infoFraisForfait = $pdo->getLesFraisForfait($_SESSION['visiteur'], $_SESSION['date']);
         $infoFraisHorsForfait = $pdo->getLesFraisHorsForfait($_SESSION['visiteur'], $_SESSION['date']);
-        $infoFraisKM = $pdo->getLesFraisKM($_SESSION['visiteur']);
         include'vues/v_FicheFraisSP.php';
         $_SESSION['montant'] = $montants;
         break;
@@ -36,8 +41,8 @@ switch ($action) {
         $pdo->validerFicheDeFraisVA($_SESSION['visiteur'], $_SESSION['date'], $_SESSION['montant']);
         ?>
         <div class = "alert alert-success" role = "alert">
-        <p>Votre fiche de frais a bien été validée ! <a href = "index.php?uc=SuivrePaiement&action=selectionnerVisiteur">Cliquez ici</a>
-        pour revenir à la page d'accueil.</p>
+            <p>Votre fiche de frais a bien été mise en paiement ! <a href = "index.php?uc=SuivrePaiement&action=selectionnerVisiteur">Cliquez ici</a>
+                pour revenir à la selection.</p>
         </div>
-        <?php
+    <?php
 }
--- controleurs/c_ValiderFicheDeFrais.php
+++ controleurs/c_ValiderFicheDeFrais.php
@@ -10,13 +10,19 @@ $idVisiteur = filter_input(INPUT_POST, 'lstVisiteur', FILTER_SANITIZE_STRING);
 $montants = 0;
 switch ($action) {
     case 'selectionnerMois' :
-        $lesMois = $pdo->getMoisFicheDeFrais();
-        // Afin de sélectionner par défaut le dernier mois dans la zone de liste
-        // on demande toutes les clés, et on prend la première,
-        // les mois étant triés décroissants
-        $lesCles = array_keys($lesMois);
-        $moisASelectionne = $lesCles[0];
-        include 'vues/v_SelectMois.php';
+        if (empty($pdo->getMoisFicheDeFrais())) {
+            ajouterErreur("Aucune fiche de frais n'est à valider");
+            include 'vues/v_erreurs.php';
+            include 'vues/v_SelectMois.php';
+        } else {
+            $lesMois = $pdo->getMoisFicheDeFrais();
+            // Afin de sélectionner par défaut le dernier mois dans la zone de liste
+            // on demande toutes les clés, et on prend la première,
+            // les mois étant triés décroissants
+            $lesCles = array_keys($lesMois);
+            $moisASelectionne = $lesCles[0];
+            include 'vues/v_SelectMois.php';
+        }
         break;
     case 'selectionnerVisiteur' :
         $leMois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
@@ -40,7 +46,7 @@ switch ($action) {
         include 'vues/v_SelectVisiteur.php';
         $nomVis = (filter_input(INPUT_POST, 'lstVisiteur', FILTER_SANITIZE_STRING));
         trim($nomVis);
-        $idVis = $pdo ->getIdFromNomVisiteur($nomVis);
+        $idVis = $pdo->getIdFromNomVisiteur($nomVis);
         $_SESSION['visiteur'] = $idVis['id'];
         $infoFicheDeFrais = $pdo->getLesInfosFicheFrais($_SESSION['visiteur'], $_SESSION['date']);
         $infoFraisForfait = $pdo->getLesFraisForfait($_SESSION['visiteur'], $_SESSION['date']);
@@ -106,12 +112,51 @@ switch ($action) {
         $infoFraisHorsForfait = $pdo->getLesFraisHorsForfait($_SESSION['visiteur'], $_SESSION['date']);
         include'vues/v_ValiderFicheDeFrais.php';
         break;
+    case 'supprimerFrais':
+        $unIdFrais = filter_input(INPUT_GET, 'idFrais', FILTER_SANITIZE_NUMBER_INT);
+        $ceMois = filter_input(INPUT_GET, 'mois', FILTER_SANITIZE_STRING);
+        $idVisiteur = filter_input(INPUT_GET, 'idVisiteur', FILTER_SANITIZE_STRING);
+        ?>
+        <div class="alert alert-info" role="alert">
+            <p><h4>Voulez vous modifier ou supprimer le frais?<br></h4>
+            <a href="index.php?uc=ValiderFicheDeFrais&action=supprimer&idFrais=<?php echo $unIdFrais ?>">Supprimer</a> 
+            ou <a href="index.php?uc=ValiderFicheDeFrais&action=reporter&idFrais=<?php echo $unIdFrais ?>&mois=<?php echo $ceMois ?>&id=<?php echo $idVisiteur ?>">Reporter</a></p>
+        </div>
+        <?php
+        break;
+    case 'supprimer':
+        $idFrais = filter_input(INPUT_GET, 'idFrais', FILTER_SANITIZE_NUMBER_INT);
+        $pdo->refuserFraisHorsForfait($idFrais);
+        ?>
+        <div class="alert alert-info" role="alert">
+            <p>Ce frais hors forfait a bien été supprimé! <a href = "index.php?uc=ValiderFicheDeFrais&action=selectionnerMois">Cliquez ici</a>
+                pour revenir à la selection.</p>
+        </div>
+        <?php
+        break;
+
+    case 'reporter':
+        $idFrais = filter_input(INPUT_GET, 'idFrais', FILTER_SANITIZE_NUMBER_INT);
+        $mois = filter_input(INPUT_GET, 'mois', FILTER_SANITIZE_STRING);
+        $moisSuivant = $pdo->getMoisSuivant($mois);
+        $idVisiteur = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
+        if ($pdo->estPremierFraisMois($idVisiteur, $moisSuivant)) {
+            $pdo->creeNouvellesLignesFrais($idVisiteur, $moisSuivant);
+        }
+        $moisAReporter = $pdo->reporterFraisHorsForfait($idFrais, $mois);
+        ?>
+        <div class="alert alert-info" role="alert">
+            <p>Ce frais hors forfait a bien été reporté au mois suivant! <a href = "index.php?uc=ValiderFicheDeFrais&action=selectionnerMois">Cliquez ici</a>
+                pour revenir à la selection.</p>
+        </div>
+        <?php
+        break;
     case 'Valider' :
         $pdo->validerFicheDeFrais($_SESSION['visiteur'], $_SESSION['date'], $_SESSION['montant']);
         ?> </br>
         <div class = "alert alert-success" role = "alert">
-        <p>Votre fiche de frais a bien été mise en paiement ! <a href = "index.php?uc=ValiderFicheDeFrais&action=selectionnerMois">Cliquez ici</a>
-        pour revenir à la page d'accueil.</p>
+            <p>Votre fiche de frais a bien été validée ! <a href = "index.php?uc=ValiderFicheDeFrais&action=selectionnerMois">Cliquez ici</a>
+                pour revenir à la selection.</p>
         </div>
-        <?php
+    <?php
 }
