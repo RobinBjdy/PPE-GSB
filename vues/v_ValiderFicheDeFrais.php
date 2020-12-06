1ae83f6abd749cc73919d5b6d2e668329da7576d
jean Eude <jean Eude@Robin>
2020-12-06 17:48:15 +0100

    Ajout du bouton supprimer et d'un test de validation

--- vues/v_ValiderFicheDeFrais.php
+++ vues/v_ValiderFicheDeFrais.php
@@ -15,13 +15,13 @@
             <?php
             foreach ($infoFicheDeFrais as $infoFiche) {
                 $date = $infoFiche['dateModif'];
-               
+
                 foreach ($infoFraisHorsForfait as $frais) {
                     $montant = $frais['montant'];
                     $montants += $montant;
                 }
                 foreach ($infoFraisForfait as $frais) {
-                    $montant = $frais['quantite']*$frais['prix'];
+                    $montant = $frais['quantite'] * $frais['prix'];
                     $montants += $montant;
                 }
                 $nbJustificatifs = $infoFiche['nbJustificatifs'];
@@ -91,61 +91,64 @@
 <form method="post" 
       action="index.php?uc=ValiderFicheDeFrais&action=CorrigerElemHorsForfait" 
       role="form">
-<div class="panel panel-info">
-    <div class="panel-heading">Eléments hors-forfait</div>
-    <table class="table table-bordered table-responsive">
-        <tr>
-            <th>Date</th>
-            <th>Libelle</th>
-            <th>Montant</th>
-            <th></th>
-        </tr>
-        <?php
-        foreach ($infoFraisHorsForfait as $frais) { 
-            $date = $frais['date'];
-            $datee = implode('-',array_reverse  (explode('/',$date))); /* transform une date fr en une date us -> 29/10/2020 en 2020-10-29*/
-            $libellehorsFrais = $frais['libelle'];
-            $montant = $frais['montant'];
-            $id = $frais['id'];
-            ?>
+    <div class="panel panel-info">
+        <div class="panel-heading">Eléments hors-forfait</div>
+        <table class="table table-bordered table-responsive">
             <tr>
-                <td><div class="form-group">
-                        <label for="date"></label>
-                        <input type="date" 
-                               name="lesDates[<?php echo $id ?>]"
-                               size="10" maxlength="15" 
-                               value="<?php echo $datee ?>">
-                    </div></td>
-                <td><div class="form-group">
-                        <label for="libelle"></label>
-                        <input type="text" 
-                               name="lesLibelles[<?php echo $id ?>]"
-                               size="15" maxlength="12" 
-                               value="<?php echo $libellehorsFrais ?>">
-                    </div></td>
-                <td><div class="form-group">
-                        <label for="montant"></label>
-                        <input type="text" 
-                               name="lesMontants[<?php echo $id ?>]"
-                               size="10" maxlength="15" 
-                               value="<?php echo $montant ?>"> €
-                    </div></td>
-                <td><input id="okElemHorsForf" name="corriger[<?php echo $id ?>]" type="submit" value="Corriger" class="btn btn-success" 
-                           accept=""role="button"> 
-                    <input id="annuler" type="reset" value="Réinitialiser" class="btn btn-danger" 
-                           accept=""role="button">
-                </td>
-
+                <th>Date</th>
+                <th>Libelle</th>
+                <th>Montant</th>
+                <th></th>
             </tr>
-        <?php } ?>
-    </table>
-</div>
+            <?php
+            foreach ($infoFraisHorsForfait as $frais) {
+                $date = $frais['date'];
+                $datee = implode('-', array_reverse(explode('/', $date))); /* transform une date fr en une date us -> 29/10/2020 en 2020-10-29 */
+                $libellehorsFrais = $frais['libelle'];
+                $montant = $frais['montant'];
+                $id = $frais['id'];
+                ?>
+                <tr>
+                    <td><div class="form-group">
+                            <label for="date"></label>
+                            <input type="date" 
+                                   name="lesDates[<?php echo $id ?>]"
+                                   size="10" maxlength="15" 
+                                   value="<?php echo $datee ?>">
+                        </div></td>
+                    <td><div class="form-group">
+                            <label for="libelle"></label>
+                            <input type="text" 
+                                   name="lesLibelles[<?php echo $id ?>]"
+                                   size="15" maxlength="40" 
+                                   value="<?php echo $libellehorsFrais ?>">
+                        </div></td>
+                    <td><div class="form-group">
+                            <label for="montant"></label>
+                            <input type="text" 
+                                   name="lesMontants[<?php echo $id ?>]"
+                                   size="10" maxlength="15" 
+                                   value="<?php echo $montant ?>"> €
+                        </div></td>
+                    <td><input id="okElemHorsForf" name="corriger[<?php echo $id ?>]" type="submit" value="Corriger" class="btn btn-success" 
+                               accept=""role="button"> 
+                        <input id="annuler" type="reset" value="Réinitialiser" class="btn btn-danger" 
+                               accept=""role="button">
+                        <a href="index.php?uc=ValiderFicheDeFrais&action=supprimerFrais&idFrais=<?php echo $id ?>&mois=<?php echo $frais['mois']?>&idVisiteur=<?php echo $_SESSION['visiteur']?> " 
+                           type="reset" class="btn btn-danger" role="button"
+                           onclick="return confirm('Voulez-vous vraiment supprimer ou reporter ce frais hors forfait?');">Supprimer</a>
+                    </td>
+
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
 </form>
 <form method="post" 
       action="index.php?uc=ValiderFicheDeFrais&action=Valider" 
       role="form">
-<input id="okFicheFrais" type="submit" value="Valider" class="btn btn-success" 
-       accept=""role="button"> 
-<input id="annuler" type="reset" value="Réinitialiser" class="btn btn-danger" 
-       accept=""role="button">
+    <input id="okFicheFrais" type="submit" value="Valider" class="btn btn-success" 
+           accept=""role="button" onclick="return confirm('Voulez-vous vraiment valider cette fiche de frais ?');"> 
+    <input id="annuler" type="reset" value="Réinitialiser" class="btn btn-danger" 
+           accept=""role="button">
 </form></br></br>
